52959526793fa16ab2a6e17be58e501a1f4fe05f
Parham Alvani <parham.alvani@gmail.com>
2017-11-14 20:53:32 +0330

    Adds rx and tx lora messages.

---
+++ lora/messages.go
@@ -0,0 +1,20 @@
+package lora
+
+// RxMessage contains payloads received from your nodes
+type RxMessage struct {
+	ApplicationID   string
+	ApplicationName string
+	NodeName        string
+	DevEUI          string
+	FPort           int
+	FCnt            int
+	Data            []byte
+}
+
+// TxMessage contains payload send to your nodes
+type TxMessage struct {
+	Reference string
+	Confirmed bool
+	FPort     int
+	Data      []byte
+}
