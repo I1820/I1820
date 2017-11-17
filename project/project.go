748ee71a663cd86814decf81a8f637bee42fcef4
Parham Alvani <parham.alvani@gmail.com>
2017-11-18 00:40:34 +0330

    Project structure was added.

---
+++ project/project.go
@@ -0,0 +1,17 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 18-11-2017
+ * |
+ * | File Name:     project.go
+ * +===============================================
+ */
+
+package project
+
+// Project represents structure of ISRC projects
+type Project struct {
+	Name string
+	ID   string
+}
