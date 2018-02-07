33f8f5a6083b57b5be3ae2e3b60a0f64ed4b6b86
Parham Alvani <parham.alvani@gmail.com>
2018-02-07 08:00:36 -0500

    Adds http tests.

---
+++ main_test.go
@@ -0,0 +1,61 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 07-02-2018
+ * |
+ * | File Name:     main_test.go
+ * +===============================================
+ */
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	h := handle()
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	d, err := json.Marshal(projectReq{Name: "Her"})
+	if err != nil {
+		t.Fatalf("Project request marshaling: %s\n", d)
+	}
+
+	resp, err := http.Post(fmt.Sprintf("%s/api/project", s.URL), "application/json", bytes.NewBuffer(d))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	if _, err := ioutil.ReadAll(resp.Body); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	h := handle()
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/api/project/%s", s.URL, "Her"), nil)
+	cli := http.Client{}
+
+	resp, err := cli.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	if _, err := ioutil.ReadAll(resp.Body); err != nil {
+		t.Fatal(err)
+	}
+}
