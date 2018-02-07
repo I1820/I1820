bbf12b6e70c2d137a60c086eca7ce643b595b57e
Parham Alvani <parham.alvani@gmail.com>
2018-02-07 09:49:49 -0500

    Adds error json.

---
+++ error.go
@@ -0,0 +1,15 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 07-02-2018
+ * |
+ * | File Name:     error.go
+ * +===============================================
+ */
+
+package main
+
+type errorRes struct {
+	Error string `json:"error"`
+}
