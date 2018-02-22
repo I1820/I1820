03c7a0f171ce1852afb5a99188470632a223570c
Parham Alvani <parham.alvani@gmail.com>
2018-02-22 07:12:32 -0500

    Adds thing url testing.

--- main_test.go
+++ main_test.go
@@ -49,6 +49,61 @@ func TestCreate(t *testing.T) {
 	}
 }
 
+func TestThingCreate(t *testing.T) {
+	h := handle()
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	d, err := json.Marshal(thingReq{Name: "Me"})
+	if err != nil {
+		t.Fatalf("Thing request marshaling: %s\n", d)
+	}
+
+	resp, err := http.Post(fmt.Sprintf("%s/api/project/%s/things", s.URL, "Her"), "application/json", bytes.NewBuffer(d))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("Response: %s", data)
+	}
+}
+
+func TestThingRemove(t *testing.T) {
+	h := handle()
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	req, _ := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/api/things/%s", s.URL, "Me"), nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("Response: %s", data)
+	}
+
+}
+
 func TestRemove(t *testing.T) {
 	h := handle()
 	s := httptest.NewServer(h)
