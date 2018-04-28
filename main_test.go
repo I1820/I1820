f82cf784d20033f21e17d8c2cb7ef20f2d56986b
Parham Alvani <parham.alvani@gmail.com>
2018-04-28 18:52:08 +0430

    Adds some http tests.

--- main_test.go
+++ main_test.go
@@ -11,11 +11,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 func TestAbout(t *testing.T) {
@@ -40,4 +43,31 @@ func TestAbout(t *testing.T) {
 	if string(body) != "18.20 is leaving us" {
 		t.Fatalf("who leaving us?! %q", body)
 	}
+}
+
+func TestThings(t *testing.T) {
+	setupDB()
+
+	h := handle()
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	resp, err := http.Get(fmt.Sprintf("%s/api/things", s.URL))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var results []bson.M
+	json.Unmarshal(body, &results)
+	t.Log(results)
+
+	if err := resp.Body.Close(); err != nil {
+		t.Fatal(err)
+	}
+
 }
