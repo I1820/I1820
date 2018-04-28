dbb2200b3c0527ebad81fc17c31ba1fb4f63b467
Parham Alvani <parham.alvani@gmail.com>
2018-04-28 18:52:44 +0430

    Adds initiation script for mongodb.

---
+++ init.js
@@ -0,0 +1,14 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 28-04-2018
+ * |
+ * | File Name:     init.js
+ * +===============================================
+ */
+
+
+db.parsed.insert({
+  thingid: 'hello',
+});
