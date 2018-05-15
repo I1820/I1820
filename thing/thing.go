3f44ff186f3dcc48885560b313526417591972ca
Parham Alvani <parham.alvani@gmail.com>
2018-05-15 22:10:19 +0430

    Corrects typo in comment.

--- thing/thing.go
+++ thing/thing.go
@@ -13,5 +13,5 @@ package thing
 // Thing contains identification and parent project
 type Thing struct {
 	ID     string `json:"id" bson:"id"`         // DevEUI
-	Status bool   `json:"status" bson:"status"` // active/deactive
+	Status bool   `json:"status" bson:"status"` // active/inactive
 }
