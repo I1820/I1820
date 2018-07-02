514527c109e598cd9f0527c6d8b7a08b19312ddc
Parham Alvani <parham.alvani@gmail.com>
2018-07-02 06:04:52 +0430

    Adds logger for action suite.

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -3,14 +3,19 @@ package actions
 import (
 	"testing"
 
+	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/suite"
 )
 
 type ActionSuite struct {
 	*suite.Action
+	buffalo.Logger
 }
 
 func Test_ActionSuite(t *testing.T) {
-	as := &ActionSuite{suite.NewAction(App())}
+	as := &ActionSuite{
+		suite.NewAction(App()),
+		buffalo.NewLogger("info"),
+	}
 	suite.Run(t, as)
 }
