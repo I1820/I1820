8a50f015d5f660223ce7dfab3d648afef3df88f4
Parham Alvani <parham.alvani@gmail.com>
2018-07-02 05:51:07 +0430

    Adds database connection.

--- actions/app.go
+++ actions/app.go
@@ -1,10 +1,13 @@
 package actions
 
 import (
+	"context"
+
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/middleware"
 	"github.com/gobuffalo/buffalo/middleware/ssl"
 	"github.com/gobuffalo/envy"
+	mgo "github.com/mongodb/mongo-go-driver/mongo"
 	"github.com/unrolled/secure"
 
 	"github.com/gobuffalo/x/sessions"
@@ -15,6 +18,7 @@ import (
 // application is being run. Default is "development".
 var ENV = envy.Get("GO_ENV", "development")
 var app *buffalo.App
+var db *mgo.Database
 
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
@@ -38,12 +42,26 @@ func App() *buffalo.App {
 		// Set the request content type to JSON
 		app.Use(middleware.SetContentType("application/json"))
 
+		// Create mongodb connection
+		url := envy.Get("DB_URL", "mongodb://127.0.0.1")
+		client, err := mgo.NewClient(url)
+		if err != nil {
+			buffalo.NewLogger("fatal").Fatalf("DB new client error: %s", err)
+		}
+		if err := client.Connect(context.Background()); err != nil {
+			buffalo.NewLogger("fatal").Fatalf("DB connection error: %s", err)
+		}
+		db = client.Database("lanserver")
+
 		if ENV == "development" {
 			app.Use(middleware.ParameterLogger)
 		}
 
 		app.GET("/about", AboutHandler)
-
+		api := app.Group("/api")
+		{
+			api.POST("/project", projectNewHandler)
+		}
 	}
 
 	return app
