100b29acce0fe41c105be8b717915ccbad245e5e
Parham Alvani <parham.alvani@gmail.com>
2018-07-03 11:55:08 +0430

    Adds list and destroy api.

--- actions/project.go
+++ actions/project.go
@@ -25,6 +25,7 @@ import (
 
 // ProjectsResource manages existing projects
 type ProjectsResource struct {
+	buffalo.Resource
 }
 
 // project request payload
@@ -33,7 +34,35 @@ type projectReq struct {
 	// TODO adds docker constraints and envs
 }
 
-func (v ProjectsResource) create(c buffalo.Context) error {
+// List gets all projects. This function is mapped to the path
+// GET /projects
+func (v ProjectsResource) List(c buffalo.Context) error {
+	ps := make([]project.Project, 0)
+
+	cur, err := db.Collection("pm").Find(c, bson.NewDocument())
+	if err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	for cur.Next(context.Background()) {
+		var p project.Project
+
+		if err := cur.Decode(&p); err != nil {
+			return c.Error(http.StatusInternalServerError, err)
+		}
+
+		ps = append(ps, p)
+	}
+	if err := cur.Close(context.Background()); err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	return c.Render(http.StatusOK, r.JSON(ps))
+}
+
+// Create adds a project to the DB and creates its docker. This function is mapped to the
+// path POST /projects
+func (v ProjectsResource) Create(c buffalo.Context) error {
 	var rq projectReq
 	if err := c.Bind(&rq); err != nil {
 		return c.Error(http.StatusBadRequest, err)
@@ -57,7 +86,9 @@ func (v ProjectsResource) create(c buffalo.Context) error {
 	return c.Render(http.StatusOK, r.JSON(p))
 }
 
-func (v ProjectsResource) show(c buffalo.Context) error {
+// Show gets the data for one project. This function is mapped to
+// the path GET /projects/{project_id}
+func (v ProjectsResource) Show(c buffalo.Context) error {
 	name := c.Param("project_id")
 
 	var p project.Project
@@ -70,6 +101,37 @@ func (v ProjectsResource) show(c buffalo.Context) error {
 		if err == mgo.ErrNoDocuments {
 			return c.Error(http.StatusNotFound, fmt.Errorf("Project %s not found", name))
 		}
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	return c.Render(http.StatusOK, r.JSON(p))
+}
+
+// Destroy deletes a project from the DB and its docker. This function is mapped
+// to the path DELETE /projects/{project_id}
+func (v ProjectsResource) Destroy(c buffalo.Context) error {
+	name := c.Param("project_id")
+
+	var p project.Project
+
+	dr := db.Collection("pm").FindOne(c, bson.NewDocument(
+		bson.EC.String("name", name),
+	))
+
+	if err := dr.Decode(&p); err != nil {
+		if err == mgo.ErrNoDocuments {
+			return c.Error(http.StatusNotFound, fmt.Errorf("Project %s not found", name))
+		}
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	if err := p.Runner.Remove(); err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	if _, err := db.Collection("pm").DeleteOne(c, bson.NewDocument(
+		bson.EC.String("name", name),
+	)); err != nil {
 		return c.Error(http.StatusInternalServerError, err)
 	}
 
