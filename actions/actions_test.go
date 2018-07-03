e9fd2298571754c32d371d44f00783b94af1bc7f
Parham Alvani <parham.alvani@gmail.com>
2018-07-03 09:46:06 +0430

    Removes buffalo logger.

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -3,19 +3,16 @@ package actions
 import (
 	"testing"
 
-	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/suite"
 )
 
 type ActionSuite struct {
 	*suite.Action
-	buffalo.Logger
 }
 
 func Test_ActionSuite(t *testing.T) {
 	as := &ActionSuite{
 		suite.NewAction(App()),
-		buffalo.NewLogger("info"),
 	}
 	suite.Run(t, as)
 }
