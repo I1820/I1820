48d756b2948ca7046209c0bdb694f174f445a91c
Parham Alvani <parham.alvani@gmail.com>
2018-07-18 00:34:57 +0430

    Adds runners handler test.

---
+++ actions/runner_test.go
@@ -0,0 +1,29 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 17-07-2018
+ * |
+ * | File Name:     runner_test.go
+ * +===============================================
+ */
+
+package actions
+
+func (as *ActionSuite) Test_RunnersHandler() {
+	// Create
+	resc := as.JSON("/api/projects").Post(projectReq{Name: pName})
+	as.Equalf(200, resc.Code, "Error: %s", resc.Body.String())
+
+	// GoRunner About
+	res := as.JSON("/api/runners/%s/about", pName).Get()
+	as.Equal(200, res.Code)
+	as.Contains(res.Body.String(), "18.20 is leaving us")
+
+	// Destroy
+	resd := as.JSON("/api/projects/%s", pName).Delete()
+	as.Equalf(200, resd.Code, "Error: %s", resd.Body.String())
+}
