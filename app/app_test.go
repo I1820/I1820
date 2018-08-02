5aeee251764b233e7478ca8a553ab14873dc5591
Parham Alvani <parham.alvani@gmail.com>
2018-08-03 00:50:00 +0430

    Adds uplink test.

---
+++ app/app_test.go
@@ -0,0 +1,51 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 03-08-2018
+ * |
+ * | File Name:     app_test.go
+ * +===============================================
+ */
+
+package app
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPipeline(t *testing.T) {
+	a := New()
+	a.Run()
+	ts := time.Now()
+
+	a.projectStream <- Data{
+		Raw:       []byte("Hello"),
+		Data:      nil,
+		Timestamp: ts,
+		ThingID:   "el-thing",
+		RxInfo:    nil,
+		TxInfo:    nil,
+		Project:   "",
+	}
+	time.Sleep(1 * time.Second)
+
+	var d Data
+	q := a.db.Collection("data").FindOne(context.Background(), bson.NewDocument(
+		bson.EC.SubDocument("timestamp", bson.NewDocument(
+			bson.EC.Time("$gte", ts),
+		)),
+		bson.EC.String("thingid", "el-thing"),
+	))
+	assert.NoError(t, q.Decode(&d))
+
+	assert.Equal(t, d.Timestamp.Unix(), ts.Unix())
+}
