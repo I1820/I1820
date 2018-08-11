b8075e47165b485ef7b0ba6306f0ac2f47deb25b
Parham Alvani <parham.alvani@gmail.com>
2018-08-11 21:58:41 +0430

    Adds actions

---
+++ actions/app.go
@@ -0,0 +1,101 @@
+package actions
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/middleware"
+	"github.com/gobuffalo/buffalo/middleware/ssl"
+	"github.com/gobuffalo/envy"
+	"github.com/unrolled/secure"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/gobuffalo/x/sessions"
+	"github.com/rs/cors"
+)
+
+// ENV is used to help switch settings based on where the
+// application is being run. Default is "development".
+var ENV = envy.Get("GO_ENV", "development")
+var app *buffalo.App
+
+// App is where all routes and middleware for buffalo
+// should be defined. This is the nerve center of your
+// application.
+func App() *buffalo.App {
+	if app == nil {
+		app = buffalo.New(buffalo.Options{
+			Env:          ENV,
+			SessionStore: sessions.Null{},
+			PreWares: []buffalo.PreWare{
+				cors.Default().Handler,
+			},
+			SessionName: "_link_session",
+		})
+		// Automatically redirect to SSL
+		app.Use(ssl.ForceSSL(secure.Options{
+			SSLRedirect:     ENV == "production",
+			SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
+		}))
+
+		// Set the request content type to JSON
+		app.Use(middleware.SetContentType("application/json"))
+		app.Use(func(next buffalo.Handler) buffalo.Handler {
+			return func(c buffalo.Context) error {
+				defer func() {
+					c.Response().Header().Set("Content-Type", "application/json")
+				}()
+
+				return next(c)
+			}
+		})
+
+		if ENV == "development" {
+			app.Use(middleware.ParameterLogger)
+		}
+
+		// Collectors
+		rds := prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Namespace: "link",
+				Name:      "request_duration_seconds",
+				Help:      "A histogram of latencies for requests.",
+			},
+			[]string{"path", "method", "code"},
+		)
+
+		prometheus.NewGoCollector()
+		prometheus.MustRegister(rds)
+
+		app.Use(func(next buffalo.Handler) buffalo.Handler {
+			return func(c buffalo.Context) error {
+				now := time.Now()
+
+				defer func() {
+					ws := c.Response().(*buffalo.Response)
+					req := c.Request()
+
+					rds.With(prometheus.Labels{
+						"path":   req.URL.String(),
+						"code":   strconv.Itoa(ws.Status),
+						"method": req.Method,
+					}).Observe(time.Since(now).Seconds())
+				}()
+
+				return next(c)
+			}
+		})
+
+		// Routes
+		app.GET("/about", AboutHandler)
+		api := app.Group("/api")
+		{
+		}
+		app.GET("/metrics", buffalo.WrapHandler(promhttp.Handler()))
+	}
+
+	return app
+}
