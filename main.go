9f4623f5c4cad920053c47ac14391fab90140c74
Parham Alvani <parham.alvani@gmail.com>
2018-08-11 22:45:28 +0430

    Adds hello message :smile:

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/I1820/pm/actions"
 )
 
 func main() {
+	fmt.Println("18.20 at Sep 07 2016 7:20 IR721")
+
 	app := actions.App()
 	if err := app.Serve(); err != nil {
 		log.Fatal(err)
