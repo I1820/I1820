bdae1d73aa0d13fdc31e2ed173f6d64884dfcb7c
Parham Alvani <parham.alvani@gmail.com>
2018-08-19 01:40:07 +0430

    Adds basis of isrc plugin.

---
+++ isrc/lib/lib.go
@@ -0,0 +1,18 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 19-08-2018
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import "github.com/I1820/link/isrc"
+
+var Model isrc.Model
