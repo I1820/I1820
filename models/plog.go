1161ce7d24b340ced5f3f93174b6ff244b76669c
Parham Alvani <parham.alvani@gmail.com>
2018-08-22 13:33:30 +0430

    Changes project log structure.

--- models/plog.go
+++ models/plog.go
@@ -14,20 +14,9 @@ import "time"
 
 // ProjectLog represents project logs
 type ProjectLog struct {
-	Time      time.Time `bson:"Time"`
-	Message   string    `bson:"Message"`
-	Code      string    `bson:"code"`
-	Job       string    `bson:"job"`
-	Project   string    `bson:"project"`
-	Component string    `bson:"component"`
-	Level     int       `bson:"Level"`
-}
-
-// LoraLog represents lora errors
-type LoraLog struct {
-	Timestamp time.Time
-	Error     string
-	Type      string
-	Project   string
-	FCnt      int
+	Time    time.Time `bson:"Time"`
+	Message string    `bson:"Message"`
+	Code    string    `bson:"code"`
+	Job     string    `bson:"job"`
+	Level   int       `bson:"Level"`
 }
