e2858a9131239aaae72a20b9383b85f7b6d2fcf2
Parham Alvani <parham.alvani@gmail.com>
2018-09-14 00:12:53 +0000

    Improves data comment.

--- data.go
+++ data.go
@@ -15,8 +15,9 @@ package types
 
 import "time"
 
-// Data represents uplink data and metadata
-// this structure is created and remains in the platform for each data
+// Data represents nodes data after parse phase that is done by protocols.
+// Each data have its raw and decoded payload and may have link quality information.
+// This structure is created and remains in the platform for each incoming data.
 type Data struct {
 	Raw       []byte      // data before decode
 	Data      interface{} // data after decode
