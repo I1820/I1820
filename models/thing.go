ec59d4bd3a7b0c3208a0710bd1a6e167c934d14c
Parham Alvani <parham.alvani@gmail.com>
2018-09-19 23:53:57 +0430

    Corrects thing model.

--- models/thing.go
+++ models/thing.go
@@ -12,11 +12,11 @@ package models
 
 // Thing contains identification and parent project of a thing
 type Thing struct {
-	ID     string   `json:"id" bson:"_id,omitempty"` // Thing unique identifier
-	Name   string   `json:"name" bson:"name"`        // Thing human readable name
+	ID     string   `json:"id" bson:"_id,omitempty"` // thing unique identifier
+	Name   string   `json:"name" bson:"name"`        // thing human readable name
 	Status bool     `json:"status" bson:"status"`    // active/inactive
 	Model  string   `json:"model" bson:"model"`      // model describes how to decode an incoming payload
-	Tokens []string `json:"tokens" bson:"tokens"`    // device access tokens that are generated based on K-Sortable Globally Unique IDs
+	Tokens []string `json:"tokens" bson:"tokens"`    // thing access tokens that are generated based on K-Sortable Globally Unique IDs
 
-	Project string `json:"project" bson:"project`
+	Project string `json:"project" bson:"project"`
 }
