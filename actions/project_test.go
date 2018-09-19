1c0e33d3165f8ccd24844ccc478bec89b819f5f1
Parham Alvani <parham.alvani@gmail.com>
2018-09-19 20:11:04 +0430

    Updates project tests.

--- actions/project_test.go
+++ actions/project_test.go
@@ -18,42 +18,38 @@ import (
 )
 
 const pName = "kj"
-const uName = "1995parham"
+
+var pID = ""
 
 func (as *ActionSuite) Test_ProjectsResource_Create_Show_Destroy() {
 	var pr models.Project
 
-	// Create (POST /api/{user_id}/projects)
-	resc := as.JSON("/api/%s/projects", uName).Post(projectReq{Name: pName})
+	// Create (POST /api/projects)
+	resc := as.JSON("/api/projects").Post(projectReq{Name: pName})
 	as.Equalf(200, resc.Code, "Error: %s", resc.Body.String())
 	resc.Bind(&pr)
+	pID = pr.ID
 
 	// check database for project existence
 	var pd models.Project
 	dr := db.Collection("projects").FindOne(context.Background(), bson.NewDocument(
-		bson.EC.String("name", pName),
-		bson.EC.String("user", uName),
+		bson.EC.String("id", pID),
 	))
 
 	as.NoError(dr.Decode(&pd))
 
 	as.Equal(pd, pr)
 
-	// Show (GET /api/{user_id}/projects/{project_id})
-	ress := as.JSON("/api/%s/projects/%s", uName, pName).Get()
+	// Show (GET /api/projects/{project_id})
+	ress := as.JSON("/api/projects/%s", pID).Get()
 	as.Equalf(200, ress.Code, "Error: %s", ress.Body.String())
 	ress.Bind(&pr)
 	pr.Inspects = nil
 
 	as.Equal(pd, pr)
 
-	// Deactivate (GET /api/{user_id}/projects/{project_id}/deactivate)
-	resa := as.JSON("/api/%s/projects/%s/deactivate", uName, pName).Get()
-	as.Equalf(200, resa.Code, "Error: %s", resa.Body.String())
-	resa.Bind(&pd)
-
-	// Destroy (DELETE /api/{user_id}/projects/{project_id})
-	resd := as.JSON("/api/%s/projects/%s", uName, pName).Delete()
+	// Destroy (DELETE /api/projects/{project_id})
+	resd := as.JSON("/api/projects/%s", pID).Delete()
 	as.Equalf(200, resd.Code, "Error: %s", resd.Body.String())
 	resd.Bind(&pr)
 
@@ -63,7 +59,7 @@ func (as *ActionSuite) Test_ProjectsResource_Create_Show_Destroy() {
 func (as *ActionSuite) Test_ProjectsResource_List() {
 	var ps []models.Project
 
-	res := as.JSON("/api/%s/projects", uName).Get()
+	res := as.JSON("/api/projects").Get()
 	as.Equalf(200, res.Code, "Error: %s", res.Body.String())
 	res.Bind(&ps)
 }
