c81397b07825befcf645afbaf2e08215e19df601
Parham Alvani <parham.alvani@gmail.com>
2018-09-20 09:11:05 +0430

    Updates assets model.

--- models/asset.go
+++ models/asset.go
@@ -16,6 +16,6 @@ package models
 // Asset is sensor or actuator that is attached to a thing.
 type Asset struct {
 	Title string `json:"title" bson:"title"`
-	Type  string `json:"type" bson:"type"`
-	Kind  string `json:"kind" bson:"kind"`
+	Type  string `json:"type" bson:"type"` // boolean, number, string, object and array
+	Kind  string `json:"kind" bson:"kind"` // sensor or actuator
 }
