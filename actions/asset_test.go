f921a7c373aa671381d99fc6182e94353b23b0ab
Parham Alvani <parham.alvani@gmail.com>
2018-09-20 09:28:38 +0430

    Adds assets test.

--- actions/asset_test.go
+++ actions/asset_test.go
@@ -12,3 +12,47 @@
  */
 
 package actions
+
+import "github.com/I1820/pm/models"
+
+const aName = "101"
+const aTitle = "Temperature"
+const aKind = "sensor"
+const aType = "number"
+
+func (as *ActionSuite) Test_AssetsResource_Create() {
+	// Create project
+	var pr models.Project
+	resc := as.JSON("/api/projects").Post(projectReq{Name: pName, Owner: pOwner})
+	as.Equalf(200, resc.Code, "Error: %s", resc.Body.String())
+	resc.Bind(&pr)
+	pID = pr.ID
+
+	// Create thing
+	var tc models.Thing
+	rest := as.JSON("/api/projects/%s/things", pID).Post(thingReq{Name: tName})
+	as.Equalf(200, rest.Code, "Error: %s", rest.Body.String())
+	rest.Bind(&tc)
+	tID = tc.ID
+
+	// Create (POST /api/things/{thing_id}/assets)
+	resa := as.JSON("/api/things/%s/assets", tID).Post(assetReq{Name: aName, Title: aTitle, Type: aType, Kind: aKind})
+	as.Equalf(200, resa.Code, "Error: %s", resa.Body.String())
+
+	// Show (Get /api/things/{thing_id}/assets/{asset_name})
+	var a models.Asset
+	ress := as.JSON("/api/things/%s/assets", tID).Post(assetReq{Name: aName, Title: aTitle, Type: aType, Kind: aKind})
+	as.Equalf(200, ress.Code, "Error: %s", ress.Body.String())
+	ress.Bind(&a)
+	as.Equal(aTitle, a.Title)
+	as.Equal(aType, a.Type)
+	as.Equal(aKind, a.Kind)
+
+	// Destroy thing
+	resr := as.JSON("/api/projects/%s/things/%s", pID, tID).Delete()
+	as.Equalf(200, resr.Code, "Error: %s", resr.Body.String())
+
+	// Destroy project
+	resd := as.JSON("/api/projects/%s", pID).Delete()
+	as.Equalf(200, resd.Code, "Error: %s", resd.Body.String())
+}
