763c931e995853b1a1e64c0fb17e058254f863d4
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 08:45:59 +0330

    Adds test for partial fetch query.

--- actions/query_test.go
+++ actions/query_test.go
@@ -10,6 +10,8 @@
 
 package actions
 
+import "time"
+
 const thingID = "5ba3f1a287a142b0a840fae1"
 const projectID = "5ba3f19c87a142b0a840fae0"
 
@@ -28,4 +30,22 @@ func (as *ActionSuite) Test_QueriesResource_List() {
 			as.Equal(r.Total, 4)
 		}
 	}
+}
+
+func (as *ActionSuite) Test_QueriesResource_PFetch() {
+	var results []pfetchResp
+
+	var req fetchReq
+	req.Range.To = time.Date(2018, time.September, 26, 0, 0, 0, 0, time.UTC)
+	req.Range.From = time.Date(2018, time.September, 30, 0, 0, 0, 0, time.UTC)
+	req.Target = "100"
+	req.Window.Size = 1
+
+	res := as.JSON("/api/projects/%s/things/%s/queries/pfetch", projectID, thingID).Post(req)
+	as.Equal(200, res.Code)
+
+	res.Bind(&results)
+
+	as.Equal(len(results), 1)
+	as.Equal(results[0].Data, 6750)
 }
