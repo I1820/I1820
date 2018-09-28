d6ccdb767ee84fa6c449d27009fef1909a0a7a3a
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 08:59:14 +0330

    Corrects database initiation script.

--- init.js
+++ init.js
@@ -9,15 +9,18 @@
  */
 /* eslint-env mongo */
 
+var thingID = "5ba3f1a287a142b0a840fae1"
+var projectID = "5ba3f19c87a142b0a840fae0"
+var collection = "data." + projectID +  "." + thingID
 
-db.data.insert({
+db.[collection].insert({
   raw: 7000,
   value: {
     number: 7000,
   },
   at: new ISODate("2018-09-26T21:52:06.443+03:30"),
-  project: "5ba3f19c87a142b0a840fae0",
-  thing_id: "5ba3f1a287a142b0a840fae1",
+  project: projectID,
+  thing_id: thingID,
   asset: "100"
 }, {
   raw: 6500,
@@ -25,8 +28,8 @@ db.data.insert({
     number: 6500,
   },
   at: new ISODate("2018-09-26T21:52:07.443+03:30"),
-  project: "5ba3f19c87a142b0a840fae0",
-  thing_id: "5ba3f1a287a142b0a840fae1",
+  project: projectID,
+  thing_id: thingID,
   asset: "100"
 }, {
   raw: 7000,
@@ -34,8 +37,8 @@ db.data.insert({
     number: 7000,
   },
   at: new ISODate("2018-09-26T21:52:08.443+03:30"),
-  project: "5ba3f19c87a142b0a840fae0",
-  thing_id: "5ba3f1a287a142b0a840fae1",
+  project: projectID,
+  thing_id: thingID,
   asset: "100"
 }, {
   raw: 6500,
@@ -43,7 +46,7 @@ db.data.insert({
     number: 6500,
   },
   at: new ISODate("2018-09-26T21:52:09.443+03:30"),
-  project: "5ba3f19c87a142b0a840fae0",
-  thing_id: "5ba3f1a287a142b0a840fae1",
+  project: projectID,
+  thing_id: thingID,
   asset: "100"
 });
