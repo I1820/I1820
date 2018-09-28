827940e421bb1bfb582d2c62a3e9a097d72f62b4
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 16:14:56 +0330

    Adds run me script.

---
+++ runme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : me.sh
+#
+# [] Creation Date : 28-09-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+# Creates basis for pm component of I1820 platform.
+# please run this script once and for all.
+docker network create -d bridge --subnet 192.168.72.0/24 --gateway 192.168.72.1 i1820
+docker pull i1820/elrunner
+docker pull redis:alpine
