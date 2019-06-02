2cf5b10ca86b8b5dbfc1faca1133290f7a36620b
Parham Alvani <parham.alvani@gmail.com>
2019-06-03 01:42:01 +0430

    feat: Create Core Application Instance in Main

--- main.go
+++ main.go
@@ -12,16 +12,35 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/I1820/link/actions"
+	"github.com/I1820/link/core"
+	"github.com/I1820/link/models/aolab"
+	"github.com/I1820/link/protocols/lan"
+	"github.com/I1820/link/protocols/lora"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	fmt.Println("18.20 at Sep 07 2016 7:20 IR721")
 
+	// load configuration
+	c := config()
+
+	// creates the core application and registers the defaults
+	linkApp, err := core.New(c.PM.URL, c.Database.URL, c.Core.Broker.Addr)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	linkApp.RegisterProtocol(lora.Protocol{})
+	linkApp.RegisterProtocol(lan.Protocol{})
+	linkApp.RegisterModel(aolab.Model{})
+	if err := linkApp.Run(); err != nil {
+		logrus.Fatal(err)
+	}
+
 	buffaloApp := actions.App()
 	if err := buffaloApp.Serve(); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
