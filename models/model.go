c2889f2686e444c8fd20efef7546e2d08a81318d
Parham Alvani <parham.alvani@gmail.com>
2019-06-03 01:36:32 +0430

    refactor: Move Model into Models Package

---
+++ models/model.go
@@ -0,0 +1,9 @@
+package models
+
+// Model is a decoder/encoder interface like generic (based on user scripts) or aolab
+type Model interface {
+	Decode([]byte) interface{}
+	Encode(interface{}) []byte
+
+	Name() string
+}
