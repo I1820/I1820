d4330e375cb4b3aa4ee23532e943c07e57017542
Parham Alvani <parham.alvani@gmail.com>
2019-06-02 20:02:59 +0430

    feat: Create Protocol Interface

---
+++ protocols/protocol.go
@@ -0,0 +1,13 @@
+package protocols
+
+import "github.com/I1820/types"
+
+// Protocol is a uplink/downlink protocol like lan or lora
+type Protocol interface {
+	TxTopic() string
+	RxTopic() string
+
+	Name() string
+
+	Marshal([]byte) (types.Data, error)
+}
