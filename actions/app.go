382bde3040b21d573530d9a86e0e8c73e9fdeff4
Parham Alvani <parham.alvani@gmail.com>
2019-06-03 11:50:59 +0430

    feat: Migrate to Echo

--- actions/app.go
+++ actions/app.go
@@ -1,109 +1,30 @@
 package actions
 
 import (
-	"strconv"
-	"time"
-
 	"github.com/I1820/link/core"
-	"github.com/gobuffalo/buffalo"
-	"github.com/gobuffalo/buffalo/middleware"
-	"github.com/gobuffalo/buffalo/middleware/ssl"
-	"github.com/gobuffalo/envy"
-	"github.com/unrolled/secure"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/gobuffalo/x/sessions"
-	"github.com/rs/cors"
+	"github.com/labstack/echo/v4"
 )
 
-// ENV is used to help switch settings based on where the
-// application is being run. Default is "development".
-var ENV = envy.Get("GO_ENV", "development")
-var app *buffalo.App
 var linkApp *core.Application
 
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
 // application.
-func App() *buffalo.App {
-	if app == nil {
-		app = buffalo.New(buffalo.Options{
-			Env:          ENV,
-			SessionStore: sessions.Null{},
-			PreWares: []buffalo.PreWare{
-				cors.Default().Handler,
-			},
-			SessionName: "_link_session",
-		})
-		// Automatically redirect to SSL
-		app.Use(ssl.ForceSSL(secure.Options{
-			SSLRedirect:     ENV == "production",
-			SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
-		}))
+func App(debug bool) *echo.Echo {
+	e := echo.New()
 
-		// Set the request content type to JSON
-		app.Use(middleware.SetContentType("application/json"))
-		app.Use(func(next buffalo.Handler) buffalo.Handler {
-			return func(c buffalo.Context) error {
-				defer func() {
-					c.Response().Header().Set("Content-Type", "application/json")
-				}()
+	// prometheus middleware
+	e.Use(NewPrometheusMiddleware("i1820_link"))
 
-				return next(c)
-			}
-		})
-		// Set the request content type to JSON
-		app.Use(middleware.SetContentType("application/json"))
-
-		if ENV == "development" {
-			app.Use(middleware.ParameterLogger)
-		}
-
-		// Collectors
-		rds := prometheus.NewHistogramVec(
-			prometheus.HistogramOpts{
-				Namespace: "link",
-				Name:      "request_duration_seconds",
-				Help:      "A histogram of latencies for requests.",
-			},
-			[]string{"path", "method", "code"},
-		)
-
-		prometheus.NewGoCollector()
-		prometheus.MustRegister(rds)
-
-		app.Use(func(next buffalo.Handler) buffalo.Handler {
-			return func(c buffalo.Context) error {
-				now := time.Now()
-
-				defer func() {
-					ws := c.Response().(*buffalo.Response)
-					req := c.Request()
-
-					rds.With(prometheus.Labels{
-						"path":   req.URL.String(),
-						"code":   strconv.Itoa(ws.Status),
-						"method": req.Method,
-					}).Observe(time.Since(now).Seconds())
-				}()
-
-				return next(c)
-			}
-		})
-
-		// Routes
-		app.GET("/about", AboutHandler)
-		api := app.Group("/api")
-		{
-			mr := ModelsResource{}
-			api.Resource("/models", mr)
-			api.POST("/send", SendHandler)
-
-		}
-		app.GET("/metrics", buffalo.WrapHandler(promhttp.Handler()))
+	// Routes
+	e.GET("/about", AboutHandler)
+	api := e.Group("/api")
+	{
+		mr := ModelsResource{}
+		e.Resource("/models", mr)
+		e.POST("/send", SendHandler)
 	}
 
-	return app
+	return e
 }
