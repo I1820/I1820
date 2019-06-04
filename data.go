ed9d0bf1899b29729f46e604204333f26027c99e
Parham Alvani <parham.alvani@gmail.com>
2019-06-04 21:37:44 +0430

    feat: Add JSON Tags

--- data.go
+++ data.go
@@ -19,15 +19,15 @@ import "time"
 // Each data have its raw and decoded payload and may have link quality information.
 // This structure is created and remains in the platform for each incoming data.
 type Data struct {
-	Raw       []byte      // data before decode
-	Data      interface{} // data after decode
-	Timestamp time.Time   // when data received in uplink
-	ThingID   string      // deveui
+	Raw       []byte      `json:"raw"`       // data before decode
+	Data      interface{} `json:"data"`      // data after decode
+	Timestamp time.Time   `json:"timestamp"` // when data received in uplink
+	ThingID   string      `json:"thing_id"`  // deveui
 
-	RxInfo interface{}
-	TxInfo interface{}
+	RxInfo interface{} `json:"rx_info"`
+	TxInfo interface{} `json:"tx_info"`
 
-	Project  string // thing project identification
-	Protocol string // uplink protocol
-	Model    string // way of decode
+	Project  string `json:"project"`  // thing project identification
+	Protocol string `json:"protocol"` // uplink protocol
+	Model    string `json:"model"`    // way of decode
 }
