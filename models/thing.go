330211493527e4b844fbfa1b9bc73b22e07f4b7b
Parham Alvani <parham.alvani@gmail.com>
2019-06-07 16:15:52 +0430

    feat: Add Thing Model

---
+++ models/thing.go
@@ -0,0 +1,9 @@
+package models
+
+// Thing contains thing identification and its parent project
+type Thing struct {
+	Name    string `json:"name" bson:"name"`     // DevEUI
+	Status  bool   `json:"status" bson:"status"` // active/inactive
+	Model   string `json:"model" bson:"model"`   // model describes project decoder
+	Project string `json:"project" bson:"model"` // project identification
+}
