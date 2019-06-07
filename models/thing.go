382d1397a5145c4fe5bd4a32696c027b44e018e2
Parham Alvani <parham.alvani@gmail.com>
2019-06-07 18:44:05 +0430

    fix: Correct Thing Model Issue

--- models/thing.go
+++ models/thing.go
@@ -2,8 +2,8 @@ package models
 
 // Thing contains thing identification and its parent project
 type Thing struct {
-	Name    string `json:"name" bson:"name"`     // DevEUI
-	Status  bool   `json:"status" bson:"status"` // active/inactive
-	Model   string `json:"model" bson:"model"`   // model describes project decoder
-	Project string `json:"project" bson:"model"` // project identification
+	Name    string `json:"name" bson:"name"`       // DevEUI
+	Status  bool   `json:"status" bson:"status"`   // active/inactive
+	Model   string `json:"model" bson:"model"`     // model describes project decoder
+	Project string `json:"project" bson:"project"` // project identification
 }
