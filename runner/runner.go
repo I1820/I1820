c8daa44bec1f5a148d560677284f5cc57bad2e89
Parham Alvani <parham.alvani@gmail.com>
2019-06-19 04:40:38 +0430

    fix: Correct Runner Name Environment

--- runner/runner.go
+++ runner/runner.go
@@ -151,7 +151,7 @@ func createRunner(ctx context.Context, name string, envs []Env) (string, string,
 
 	dockerEnvs := []string{
 		fmt.Sprintf("REDIS_HOST=rd_%s", name),
-		fmt.Sprintf("PROJECT=%s", name),
+		fmt.Sprintf("NAME=%s", name),
 		"PORT=8080",
 		"ADDR=0.0.0.0",
 	}
