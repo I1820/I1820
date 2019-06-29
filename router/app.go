91365941bfae389473610010b2b51ef646db1a10
Parham Alvani <parham.alvani@gmail.com>
2019-06-29 14:08:35 +0000

    refactor: Create a Router Module to Setup a General Instance of Echo

---
+++ router/app.go
@@ -0,0 +1,28 @@
+package actions
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+// App creates new instance of Echo and configures it
+func App(debug bool, name string) *echo.Echo {
+	app := echo.New()
+	app.Use(middleware.Logger())
+	app.Use(middleware.Recover())
+	app.Pre(middleware.RemoveTrailingSlash())
+
+	if debug {
+		app.Logger.SetLevel(log.DEBUG)
+	}
+
+	// validator
+	app.Validator = &DefaultValidator{validator.New()}
+
+	// prometheus middleware
+	app.Use(NewPrometheusMiddleware(name))
+
+	return app
+}
