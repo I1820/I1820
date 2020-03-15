dce9a15c4b92ff3cd8a13e613da240eea3f6ae9c
Parham Alvani <parham.alvani@gmail.com>
2020-03-15 20:36:06 +0330

    feat: Add Database Package

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/I1820/link/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionTimeout = 10 * time.Second
+
+// New creates a new mongodb connection and tests it
+func New(cfg config.Database) (*mongo.Database, error) {
+	// create mongodb connection
+	client, err := mongo.NewClient(options.Client().ApplyURI(cfg.URL))
+	if err != nil {
+		return nil, fmt.Errorf("db new client error: %w", err)
+	}
+
+	// connect to the mongodb
+	ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
+	defer done()
+
+	if err := client.Connect(ctx); err != nil {
+		return nil, fmt.Errorf("db connection error: %w", err)
+	}
+
+	return client.Database(cfg.Name), nil
+}
