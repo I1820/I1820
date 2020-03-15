4b8ed2db52dd7c0a94999be4ef5c1189ef79006e
Parham Alvani <parham.alvani@gmail.com>
2020-03-15 21:08:11 +0330

    feat: Add Data Store

---
+++ store/data.go
@@ -0,0 +1,29 @@
+package store
+
+import (
+	"context"
+
+	"github.com/I1820/types"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// Collection is mongodb collection name for data
+const Collection  =  "data"
+
+type Data struct {
+	DB *mongo.Database
+}
+
+func New(db *mongo.Database) *Data {
+	return &Data{
+		DB:db,
+	}
+}
+
+// Insert given instance of data into database
+func (d *Data) Insert(ctx context.Context, i types.Data) error {
+	if _, err := d.DB.Collection("data").InsertOne(ctx, i); err != nil {
+		return err
+	}
+	return nil
+}
