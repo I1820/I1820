2b9b7d8d41925f20feecd53e899e3dc91d86760f
Parham Alvani <parham.alvani@gmail.com>
2020-03-21 21:33:04 +0430

    fix: Correct Lint Issue

--- request/thing.go
+++ request/thing.go
@@ -5,6 +5,14 @@ import (
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
+const (
+	minLatitude = -90.0
+	maxLatitude = 90.0
+
+	minLongitude = 0
+	maxLongitude = 180
+)
+
 // Location contains latitude and longitude for representing a location
 type Location struct {
 	Latitude  float64 `json:"lat"`
@@ -13,8 +21,8 @@ type Location struct {
 
 func (l Location) Validate() error {
 	return validation.ValidateStruct(&l,
-		validation.Field(&l.Latitude, validation.Min(-90.0), validation.Max(90.0)),
-		validation.Field(&l.Longitude, validation.Min(0.0), validation.Max(180.0)),
+		validation.Field(&l.Latitude, validation.Min(minLatitude), validation.Max(maxLatitude)),
+		validation.Field(&l.Longitude, validation.Min(minLongitude), validation.Max(maxLongitude)),
 	)
 }
 
