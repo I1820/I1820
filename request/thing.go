3c515aab6195d3cdcc7890a26837935178b0022b
Parham Alvani <parham.alvani@gmail.com>
2020-03-21 21:11:33 +0430

    fix: Corret Thing Request Validation

--- request/thing.go
+++ request/thing.go
@@ -5,21 +5,30 @@ import (
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
+// Location contains latitude and longitude for representing a location
+type Location struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"long"`
+}
+
+func (l Location) Validate() error {
+	return validation.ValidateStruct(&l,
+		validation.Field(&l.Latitude, is.Latitude),
+		validation.Field(&l.Longitude, is.Longitude),
+	)
+}
+
 // Thing is a request payload for creating thing
 type Thing struct {
 	Name     string `json:"name"`
 	Model    string `json:"model"`
-	Location struct {
-		Latitude  float64 `json:"lat"`
-		Longitude float64 `json:"long"`
-	} `json:"location"`
+	Location `json:"location"`
 }
 
 func (t Thing) Validate() error {
 	return validation.ValidateStruct(&t,
 		validation.Field(&t.Name, validation.Required),
 		validation.Field(&t.Model, is.Alphanumeric),
-		validation.Field(&t.Location.Latitude, is.Latitude),
-		validation.Field(&t.Location.Longitude, is.Longitude),
+		validation.Field(&t.Location),
 	)
 }
---
+++ request/thing_test.go
@@ -0,0 +1,52 @@
+package request
+
+import "testing"
+
+func TestThing_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		fields  Thing
+		wantErr bool
+	}{
+		{
+			name: "with model",
+			fields: Thing{
+				Name:  "raha",
+				Model: "aolab",
+			},
+			wantErr: false,
+		},
+		{
+			name: "without model",
+			fields: Thing{
+				Name: "raha",
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid location",
+			fields: Thing{
+				Name: "raha",
+				Location: Location{
+					Latitude:  1024,
+					Longitude: 30,
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	// nolint: scopelint
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			thing := Thing{
+				Name:     tt.fields.Name,
+				Model:    tt.fields.Model,
+				Location: tt.fields.Location,
+			}
+			if err := thing.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
