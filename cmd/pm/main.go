84c765043ec56091de07787b7bd1c46898fad545
Parham Alvani <parham.alvani@gmail.com>
2020-03-22 14:47:54 +0430

    feat: Add PM Command

---
+++ cmd/pm/main.go
@@ -0,0 +1,83 @@
+package pm
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/I1820/I1820/config"
+	"github.com/I1820/I1820/db"
+	"github.com/I1820/I1820/handler"
+	"github.com/I1820/I1820/router"
+	"github.com/I1820/I1820/runner"
+	"github.com/I1820/I1820/store"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+const (
+	// ExitTimeout is a time that application waits for API service to exit
+	ExitTimeout = 5 * time.Second
+)
+
+func main(cfg config.Config) {
+	e := router.App()
+
+	db, err := db.New(cfg.Database)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	m, err := runner.New()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	rh := handler.Runner{
+		Store: store.Project{
+			DB: db,
+		},
+		Manager:    m,
+		DockerHost: cfg.Docker.Host,
+	}
+
+	api := e.Group("/api")
+	{
+		rh.Register(api)
+	}
+
+	go func() {
+		if err := e.Start(fmt.Sprintf(":%d", config.PMPort)); err != http.ErrServerClosed {
+			logrus.Fatalf("API Service failed with %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), ExitTimeout)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		log.Printf("API Service failed on exit: %s", err)
+	}
+}
+
+// Register tm command
+func Register(root *cobra.Command, cfg config.Config) {
+	root.AddCommand(
+		&cobra.Command{
+			Use:   "pm",
+			Short: "Who manages your project and their dockers",
+			Run: func(cmd *cobra.Command, args []string) {
+				main(cfg)
+			},
+		},
+	)
+}
