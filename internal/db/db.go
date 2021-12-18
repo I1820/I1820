77b5fc35bd0486389d867857f7ac3d3e76d29a79
Parham Alvani <parham.alvani@gmail.com>
2021-12-18 09:18:20 +0330

    feat: update db package

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,16 +6,17 @@ import (
 	"reflect"
 	"time"
 
-	"github.com/I1820/I1820/config"
+	"github.com/I1820/I1820/internal/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/bsontype"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 const connectionTimeout = 10 * time.Second
 
-// New creates a new mongodb connection and tests it
+// New creates a new mongodb connection and tests it.
 func New(cfg config.Database) (*mongo.Database, error) {
 	// register custom codec registry to handle empty interfaces
 	rb := bson.NewRegistryBuilder()
@@ -27,12 +28,24 @@ func New(cfg config.Database) (*mongo.Database, error) {
 		return nil, fmt.Errorf("db new client error: %w", err)
 	}
 
-	// connect to the mongodb
-	ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
-	defer done()
+	{
+		// connect to the mongodb
+		ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
+		defer done()
 
-	if err := client.Connect(ctx); err != nil {
-		return nil, fmt.Errorf("db connection error: %w", err)
+		if err := client.Connect(ctx); err != nil {
+			return nil, fmt.Errorf("db connection error: %w", err)
+		}
+	}
+
+	{
+		// connect to the mongodb
+		ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
+		defer done()
+
+		if err := client.Ping(ctx, readpref.Primary()); err != nil {
+			return nil, fmt.Errorf("db primary ping error: %w", err)
+		}
 	}
 
 	return client.Database(cfg.Name), nil
