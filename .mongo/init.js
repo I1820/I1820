5218e972cec2b156426973d399064ea978d40be9
Parham Alvani <parham.alvani@gmail.com>
2021-12-18 10:18:57 +0330

    fix: correct mongo initiation

--- .mongo/init.js
+++ .mongo/init.js
@@ -9,75 +9,106 @@
  */
 /* eslint-env mongo */
 
-var projectID = "el-project";
-var collection = "data";
+const projectID = "el-project";
+const collection = "data";
 
-db[collection].insert([{
-  raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
-  data: {
-    100: 7000,
-    101: 6606,
-    count: "17"
+db = db.getSiblingDB("i1820");
+
+db[collection].insert([
+  {
+    raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
+    data: {
+      100: 7000,
+      101: 6606,
+      count: "17",
+    },
+    timestamp: new ISODate("2018-11-11T09:47:20.902Z"),
+    thingid: "0000000000000073",
+    rxinfo: [
+      {
+        mac: "b827ebffff70c80a",
+        name: "5cf0f70f7064c500094b5e31",
+        time: new ISODate("0001-01-01T00:00:00Z"),
+        rssi: new NumberLong(-57),
+        lorasnr: 7,
+      },
+    ],
+    txinfo: {
+      frequency: new NumberLong(868300000),
+      adr: false,
+      coderate: "4/5",
+    },
+    project: projectID,
   },
-  timestamp: new ISODate("2018-11-11T09:47:20.902Z"),
-  thingid : "0000000000000073",
-  rxinfo: [
-    { mac : "b827ebffff70c80a", name : "5cf0f70f7064c500094b5e31", time : new ISODate("0001-01-01T00:00:00Z"), rssi : new NumberLong(-57), lorasnr : 7 }
-  ],
-  txinfo: {
-    frequency: new NumberLong(868300000),
-    adr: false,
-    coderate: "4/5"
+  {
+    raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
+    data: {
+      100: 7000,
+      101: 6606,
+      count: "18",
+    },
+    timestamp: new ISODate("2018-11-11T09:47:21.902Z"),
+    thingid: "0000000000000073",
+    rxinfo: [
+      {
+        mac: "b827ebffff70c80a",
+        name: "5cf0f70f7064c500094b5e31",
+        time: new ISODate("0001-01-01T00:00:00Z"),
+        rssi: new NumberLong(-57),
+        lorasnr: 7,
+      },
+    ],
+    txinfo: {
+      frequency: new NumberLong(868300000),
+      adr: false,
+      coderate: "4/5",
+    },
+    project: projectID,
   },
-  project : projectID,
-}, {
-  raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
-  data: {
-    100: 7000,
-    101: 6606,
-    count: "18"
+  {
+    raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
+    data: {
+      100: 7000,
+      101: 6606,
+      count: "19",
+    },
+    timestamp: new ISODate("2018-11-11T09:47:22.902Z"),
+    thingid: "0000000000000073",
+    rxinfo: [
+      {
+        mac: "b827ebffff70c80a",
+        name: "5cf0f70f7064c500094b5e31",
+        time: new ISODate("0001-01-01T00:00:00Z"),
+        rssi: new NumberLong(-57),
+        lorasnr: 7,
+      },
+    ],
+    txinfo: {
+      frequency: new NumberLong(868300000),
+      adr: false,
+      coderate: "4/5",
+    },
+    project: projectID,
   },
-  timestamp: new ISODate("2018-11-11T09:47:21.902Z"),
-  thingid : "0000000000000073",
-  rxinfo: [
-    { mac : "b827ebffff70c80a", name : "5cf0f70f7064c500094b5e31", time : new ISODate("0001-01-01T00:00:00Z"), rssi : new NumberLong(-57), lorasnr : 7 }
-  ],
-  txinfo: {
-    frequency: new NumberLong(868300000),
-    adr: false,
-    coderate: "4/5"
+  {
+    raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
+    data: null,
+    timestamp: new ISODate("2018-11-11T09:47:23.902Z"),
+    thingid: "0000000000000073",
+    rxinfo: [
+      {
+        mac: "b827ebffff70c80a",
+        name: "5cf0f70f7064c500094b5e31",
+        time: new ISODate("0001-01-01T00:00:00Z"),
+        rssi: new NumberLong(-57),
+        lorasnr: 7,
+      },
+    ],
+    txinfo: {
+      frequency: new NumberLong(868300000),
+      adr: false,
+      coderate: "4/5",
+    },
+    project: projectID,
   },
-  project : projectID,
-}, {
-  raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
-  data: {
-    100: 7000,
-    101: 6606,
-    count: "19"
-  },
-  timestamp: new ISODate("2018-11-11T09:47:22.902Z"),
-  thingid : "0000000000000073",
-  rxinfo: [
-    { mac : "b827ebffff70c80a", name : "5cf0f70f7064c500094b5e31", time : new ISODate("0001-01-01T00:00:00Z"), rssi : new NumberLong(-57), lorasnr : 7 }
-  ],
-  txinfo: {
-    frequency: new NumberLong(868300000),
-    adr: false,
-    coderate: "4/5"
-  },
-  project : projectID,
-}, {
-  raw: new BinData(0, "o2MxMDAZG1hjMTAxGRnOZWNvdW50YjE3"),
-  data: null,
-  timestamp: new ISODate("2018-11-11T09:47:23.902Z"),
-  thingid : "0000000000000073",
-  rxinfo: [
-    { mac : "b827ebffff70c80a", name : "5cf0f70f7064c500094b5e31", time : new ISODate("0001-01-01T00:00:00Z"), rssi : new NumberLong(-57), lorasnr : 7 }
-  ],
-  txinfo: {
-    frequency: new NumberLong(868300000),
-    adr: false,
-    coderate: "4/5"
-  },
-  project : projectID,
-}]);
+]);
