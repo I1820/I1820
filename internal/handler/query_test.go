2af0d29d9d4e23b94d4288169c5057e387aeb287
Parham Alvani <parham.alvani@gmail.com>
2021-12-18 10:18:42 +0330

    fix: correct packages

--- internal/handler/query_test.go
+++ internal/handler/query_test.go
@@ -1,4 +1,4 @@
-package handler
+package handler_test
 
 import (
 	"bytes"
@@ -8,15 +8,19 @@ import (
 	"net/http/httptest"
 	"time"
 
-	"github.com/I1820/I1820/model"
-	"github.com/I1820/I1820/request"
+	"github.com/I1820/I1820/internal/model"
+	"github.com/I1820/I1820/internal/request"
 	"github.com/labstack/echo/v4"
 )
 
-const thingID = "0000000000000073"
-const projectID = "el-project"
+const (
+	thingID   = "0000000000000073"
+	projectID = "el-project"
+)
 
 func (suite *Suite) TestQueriesHandlerList() {
+	require := suite.Require()
+
 	var results map[string]int
 
 	w := httptest.NewRecorder()
@@ -24,14 +28,14 @@ func (suite *Suite) TestQueriesHandlerList() {
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	suite.engine.ServeHTTP(w, req)
 
-	suite.Equal(http.StatusOK, w.Code, w.Body.String())
-
-	suite.NoError(json.Unmarshal(w.Body.Bytes(), &results))
-
-	suite.Equal(results[thingID], 4)
+	require.Equal(http.StatusOK, w.Code, w.Body.String())
+	require.NoError(json.Unmarshal(w.Body.Bytes(), &results))
+	require.Equal(results[thingID], 4)
 }
 
 func (suite *Suite) TestQueriesHandlerFetch() {
+	require := suite.Require()
+
 	var results []model.Data
 
 	var freq request.Fetch
@@ -51,21 +55,22 @@ func (suite *Suite) TestQueriesHandlerFetch() {
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	suite.engine.ServeHTTP(w, req)
 
-	suite.Equal(http.StatusOK, w.Code, w.Body.String())
+	require.Equal(http.StatusOK, w.Code, w.Body.String())
+	require.NoError(json.Unmarshal(w.Body.Bytes(), &results))
+	require.Equal(4, len(results))
 
-	suite.NoError(json.Unmarshal(w.Body.Bytes(), &results))
+	require.Equal(nil, results[0].Data)
 
-	suite.Equal(4, len(results))
-
-	suite.Equal(nil, results[0].Data)
 	record := results[1]
-	suite.Equal(thingID, record.ThingID)
-	suite.Equal(7000.0, record.Data.(map[string]interface{})["100"])
-	suite.Equal(6606.0, record.Data.(map[string]interface{})["101"])
-	suite.Equal("19", record.Data.(map[string]interface{})["count"])
+	require.Equal(thingID, record.ThingID)
+	require.Equal(7000.0, record.Data.(map[string]interface{})["100"])
+	require.Equal(6606.0, record.Data.(map[string]interface{})["101"])
+	require.Equal("19", record.Data.(map[string]interface{})["count"])
 }
 
 func (suite *Suite) TestQueriesHandlerFetchSingle() {
+	require := suite.Require()
+
 	var results []model.Data
 
 	since := time.Date(2017, time.September, 11, 0, 0, 0, 0, time.UTC).Unix()
@@ -80,16 +85,15 @@ func (suite *Suite) TestQueriesHandlerFetchSingle() {
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	suite.engine.ServeHTTP(w, req)
 
-	suite.Equal(http.StatusOK, w.Code, w.Body.String())
+	require.Equal(http.StatusOK, w.Code, w.Body.String())
+	require.NoError(json.Unmarshal(w.Body.Bytes(), &results))
+	require.Equal(4, len(results))
 
-	suite.NoError(json.Unmarshal(w.Body.Bytes(), &results))
+	require.Equal(nil, results[0].Data)
 
-	suite.Equal(4, len(results))
-
-	suite.Equal(nil, results[0].Data)
 	record := results[1]
-	suite.Equal(thingID, record.ThingID)
-	suite.Equal(7000.0, record.Data.(map[string]interface{})["100"])
-	suite.Equal(6606.0, record.Data.(map[string]interface{})["101"])
-	suite.Equal("19", record.Data.(map[string]interface{})["count"])
+	require.Equal(thingID, record.ThingID)
+	require.Equal(7000.0, record.Data.(map[string]interface{})["100"])
+	require.Equal(6606.0, record.Data.(map[string]interface{})["101"])
+	require.Equal("19", record.Data.(map[string]interface{})["count"])
 }
